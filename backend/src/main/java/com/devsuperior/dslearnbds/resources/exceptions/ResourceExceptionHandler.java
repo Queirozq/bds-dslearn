baaed84c9365b7db9f4584ee91d2a50b819506e5
Matheus Queiroz <scririft@gmail.com>
2021-10-14 23:35:35 -0300

    Exceptions, validation, security

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,41 @@
+package com.devsuperior.dslearnbds.resources.exceptions;
+import com.devsuperior.dslearnbds.services.exceptions.DatabaseException;
+import com.devsuperior.dslearnbds.services.exceptions.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.Instant;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request){
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(), "Não Encontrado", e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError err = new StandardError(Instant.now(), status.value(), "Violação de Integridade", e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request){
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationError error = new ValidationError(Instant.now(), status.value(), "Validation exception", "Erro de validação", request.getRequestURI());
+        for(FieldError x : e.getBindingResult().getFieldErrors()){
+            error.addError(x.getField(), x.getDefaultMessage());
+        }
+        return ResponseEntity.status(status).body(error);
+    }
+}
---
+++ backend/src/main/java/com/devsuperior/dslearnbds/services/UserService.java
@@ -0,0 +1,32 @@
+package com.devsuperior.dslearnbds.services;
+
+import com.devsuperior.dslearnbds.entities.User;
+import com.devsuperior.dslearnbds.repositories.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+
+
+    private static Logger logger = LoggerFactory.getLogger(UserService.class);
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        User user = repository.findByEmail(s);
+        if(user == null){
+            logger.error("Usuário não encontrado migão = " + s);
+            throw new UsernameNotFoundException("Email não encontrado");
+        }
+        logger.info("Usuário encontrado = " + s);
+        return user;
+    }
+}

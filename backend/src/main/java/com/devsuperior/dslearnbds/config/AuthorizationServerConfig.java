1de523f402e11dd84e526897ab5832bde8b47086
Matheus Queiroz <scririft@gmail.com>
2021-10-15 18:27:45 -0300

    Refresh token

--- backend/src/main/java/com/devsuperior/dslearnbds/config/AuthorizationServerConfig.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/config/AuthorizationServerConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -45,6 +46,10 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Autowired
     private JwtTokenEnhancer tokenEnhancer;
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
         security.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
@@ -56,8 +61,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 .withClient(clientid)
                 .secret(bCryptPasswordEncoder.encode(clientSecret))
                 .scopes("read", "write")
-                .authorizedGrantTypes("password")
-                .accessTokenValiditySeconds(tokenValidation);
+                .authorizedGrantTypes("password", "refresh_token")
+                .accessTokenValiditySeconds(tokenValidation)
+                .refreshTokenValiditySeconds(tokenValidation);
     }
 
     @Override
@@ -69,6 +75,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
         endpoints.authenticationManager(authenticationManager)
                  .tokenStore(jwtTokenStore)
                  .accessTokenConverter(jwtAccessTokenConverter)
-                 .tokenEnhancer(chain);
+                 .tokenEnhancer(chain)
+                 .userDetailsService(userDetailsService);
     }
 }
